d98e746b01401e524704736e2da05753a1c6d931
ahmed-ayman <terabyte055@gmail.com>
2017-06-24 05:59:22 +0200

    first commit

---
+++ projects/weathernews/app/src/main/java/data/remote/RetrofitClient.java
@@ -0,0 +1,22 @@
+package data.remote;
+
+/**
+ * Created by ahmed-ayman on 6/21/17.
+ */
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
